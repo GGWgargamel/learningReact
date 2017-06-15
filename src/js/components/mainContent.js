c49f1dc2c9fce7db1f97fc1f480c4ef3cdb68c8f
guangenwei <13671041560@163.com>
2017-06-15 17:43:29 +0800

    使用state

--- src/js/components/mainContent.js
+++ src/js/components/mainContent.js
@@ -4,10 +4,31 @@
 import React from 'react';
 
 export default class ComponentMain extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            userName:"parry"
+        };
+    }
     render(){
+        var userName="Parry";
+        var boolen=true;
+        var html="1"
+        var waa="2";
+        var that=this;
+        function changeState(){
+            that.setState({
+                userName:"hello"
+            })
+        }
+
         return(
             <div>
                 <h1>主要内容</h1>
+                <p>{this.state.userName==""?"用户":this.state.userName}</p>
+                <button  onClick={changeState}>哈哈</button>
+                <p>{html}</p>{/*需要unicode转码*/}
+                <p dangerouslySetInnerHTML={{__html:html}}></p>
             </div>
         )
     }
