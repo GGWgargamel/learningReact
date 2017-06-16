443c4ff5fdea7593aea11606baef4372fcfcf243
guangenwei <13671041560@163.com>
2017-06-16 11:00:07 +0800

    refs

--- src/js/components/mainChild.js
+++ src/js/components/mainChild.js
@@ -2,13 +2,20 @@
  * Created by admin on 2017/6/15.
  */
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 export default class bodyChild extends React.Component{
+    handleEvent(){
+        var h1=document.getElementById("h1");
+        ReactDOM.findDOMNode(h1).style.color="red"
+        console.log(this.refs.h1);
+        this.refs.h1.style.fontSize="70px";
+    }
     render(){
         return(
             <div>
                 <p onClick={this.props.handleChildValue} className="childs">子页面</p>
-                <h1>{this.props.userId}</h1>
+                <h1 id="h1" onClick={this.handleEvent.bind(this)}  ref="h1">{this.props.userId}</h1>
             </div>
         )
     }
